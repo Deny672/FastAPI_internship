b4f8aff53c789de55e898e296fb56ad4535b6db9
Denys Vusyk <denis06699@gmail.com>
2024-07-03 21:03:00 +0300

    Test created with TestClient, update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
 FROM python:3.11.9
 
-WORKDIR /app
+COPY . .
 
-COPY ./ /app
-
-COPY ./requirements_dev.txt /app/requirements.txt
-
-RUN pip install --no-cache-dir --upgrade -r requirements.txt
+RUN pip install --no-cache-dir --upgrade -r requirements_dev.txt
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,13 @@
-import pytest
-from httpx import AsyncClient, ASGITransport
-
+from fastapi.testclient import TestClient
 from app.main import app
 
 
-@pytest.mark.anyio
-async def test_root():
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        response = await ac.get("/")
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {
       "status_code": 200,
